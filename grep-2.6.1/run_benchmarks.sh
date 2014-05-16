0d3e1b313fc6ffd6d7daf973e6760b312ef8e9bc
Parfunc Group Account <rrnewton@indiana.edu>
2014-05-15 21:43:08 -0400

    Adding benchmark script for grep

---
+++ grep-2.6.1/run_benchmarks.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+BUILD_HOME=`pwd`/../..
+GREP_HOME=`pwd`
+ZCA_SRC=$BUILD_HOME/zcatoggle/src
+DYN_SRC=$BUILD_HOME/dynaprof/src
+
+echo -e "\n========================="
+echo -e "Running without profiling"
+echo -e "========================="
+
+cp -R original/ instrumented/
+cd instrumented
+
+echo -e "Building grep..."
+autoreconf
+./configure CC=gcc
+make
+
+echo -e "\nRunning grep..."
+(time ./grep -rl test . > /dev/null) 2> ../original_time
+cd ..
+
+echo -e "Cleaning up..."
+rm -rf instrumented
+
+echo -e "Done..."
+
+echo -e "\n========================="
+echo -e "Running with dynaprof    "
+echo -e "========================="
+
+cp -R original/ instrumented/
+
+echo -e "Building dynaprof...\n"
+cd $ZCA_SRC
+make clean
+make
+cd $DYN_SRC
+make clean
+make
+
+echo -e "\nInstalled dynaprof..."
+cd $GREP_HOME
+cd instrumented
+autoreconf
+./configure CC=icc LIBS='libdynaprof.a libzca-toggle.a -lelf' CFLAGS='-O0'
+make
+
+echo -e "\nInstrumenting ..."
+cd ..; ./instrument.sh
+
+echo -e "\nBuilding grep with instrumentation..."
+cd instrumented
+make clean
+make
+
+echo -e "\nRunning instrumented binary..."
+(time ./grep -rl test . > /dev/null) 2> ../dynaprof_time
+cp prof.out ..
+cd ..
+
+echo -e "\nCleaning up..."
+rm -rf instrumented
+
+echo -e "\nDone..."
+
+echo -e "\n========================="
+echo -e "Running with gprof       "
+echo -e "========================="
+
+cp -R original/ instrumented/
+cd instrumented
+
+echo -e "Building grep with gprof enabled..."
+autoreconf
+./configure CC=gcc CFLAGS='-pg'
+make
+
+echo -e "\nRunning grep..."
+(time ./grep -rl test . > /dev/null) 2> ../gprof_time
+gprof gzip gmon.out > ../gprof.out
+cd ..
+
+echo -e "Cleaning up..."
+rm -rf instrumented
+
+echo -e "Done..."
+
