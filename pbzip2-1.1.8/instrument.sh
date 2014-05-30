87a0e9ece80cf25d36ab6872a5af4aef073b68a7
Parfunc Group Account <parfunc@indiana.edu>
2014-05-30 08:45:01 -0400

    Adding instrument script for pbzip2

---
+++ pbzip2-1.1.8/instrument.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+# set -x
+ORIG=sources
+PARSE=cparse
+INSTRUMENT=tau_instrumentor
+
+cd $ORIG/
+SRC=`find ./ -name "*.c"`
+HDRS=`find ./ -name "*.h"`
+cd ..
+
+for path in $HDRS; do
+  dirnm=`dirname $path`
+  cp -v ./$ORIG/$path ./instrumented/$dirnm/
+done
+
+for path in $SRC; do 
+  dirnm=`dirname $path`
+  filenm=`basename $path`
+  fn="${filenm%.*}"
+  cp -v ./$ORIG/$path ./instrumented/$dirnm/
+  $PARSE ./instrumented/$path -I./instrumented/lib/ -o ./instrumented/$dirnm/$fn.pdb   
+  $INSTRUMENT ./instrumented/$dirnm/$fn.pdb ./instrumented/$path -l -o ./instrumented/$dirnm/$fn.tau.c \
+               -c -spec ./TAUINST.SPEC
+  mv ./instrumented/$dirnm/$fn.tau.c ./instrumented/$path
+done
+
+rm ./instrumented/$dirnm/*.pdb
