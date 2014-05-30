43cc9defc0f933792523d3ea10dd1111be45f812
Parfunc Group Account <parfunc@indiana.edu>
2014-05-30 10:21:38 -0400

    Removing old gzip version

--- gzip/instrumented/crypt.c
+++
@@ -1,8 +0,0 @@
-
-#line 1 "./instrumented/./crypt.c"
-/* crypt.c (dummy version) -- do not perform encryption
- * Hardly worth copyrighting :-)
- */
-#ifdef RCSID
-static char rcsid[] = "$Id: crypt.c,v 0.6 1993/03/22 09:48:47 jloup Exp $";
-#endif
