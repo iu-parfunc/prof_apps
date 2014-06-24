3e396009358477f5fcc164ed83ce6c1887a1db4c
Parfunc Group Account <parfunc@indiana.edu>
2014-06-24 07:25:18 -0400

    Adding pin instrumentation to benchmarks and changing the tested optimization level to be -O2

---
+++ gzip-1.6/pin/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -d "../instrumented" ]; then
+  rm -rf ../instrumented
+fi
+
+if [ ! -e ../../resources/a ]; then
+  cd ../../resources
+  chmod u+x filegen
+  ./filegen
+  cd ../gzip-1.6/unprofiled
+fi
+
+cp -R ../original/ ../instrumented/
