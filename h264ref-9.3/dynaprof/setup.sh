c4eb64c26bb98551c225a37b4c9b6471a8188812
Parfunc Group Account <parfunc@indiana.edu>
2014-07-17 13:26:17 -0400

    More changes to enviornment parameter swithcing

--- h264ref-9.3/dynaprof/setup.sh
+++ h264ref-9.3/dynaprof/setup.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 
-BENCH_HOME=`pwd`
-BUILD_HOME=`pwd`/../../../
-ZCA_SRC=$BUILD_HOME/zcatoggle/src
-DYN_SRC=$BUILD_HOME/dynaprof/src
-
 if [ -d "../instrumented" ]; then
   rm -rf ../instrumented
 fi
 
-echo -e "Building dynaprof...\n"
-cd $ZCA_SRC
-make clean
-make
-cd $DYN_SRC
-make clean
-make
-
-cd $BENCH_HOME
 cp -R ../original ../instrumented
 chmod u+x ../../install.sh
 cd ../../ ;\
