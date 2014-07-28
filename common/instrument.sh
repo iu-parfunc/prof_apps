ec6ea1857ae1ef2fe6b66c4ebcd00a85ba130348
Ryan Newton <rrnewton@gmail.com>
2014-07-28 17:22:11 -0400

    Switch to absolute pathhs in common/instrument.sh

--- common/instrument.sh
+++ common/instrument.sh
@@ -4,19 +4,21 @@ set -xe
 
 echo "Starting instrumentation, current dir `pwd`."
 
-ORIG=sources
+ROOT=`pwd`
+ORIG=$ROOT/sources
+DEST=$ROOT/instrumented
 PARSE=cparse
 INSTRUMENT=tau_instrumentor
 OPTS='-Wall -Winline -DSPEC_CPU -DPERL_CORE -DSPEC_CPU_LP64 -DSPEC_CPU_LINUX_X64'
 
-cd $ORIG/
+cd $ORIG
 SRC=`find ./ -name "*.c" -o -name "*.cpp"`
 HDRS=`find ./ -name "*.h" -o -name "*.hpp" -o -name "*.hin"`
-cd ..
+cd $ROOT
 
 for path in $HDRS; do
   dirnm=`dirname $path`
-  cp -v ./$ORIG/$path ./instrumented/$dirnm/
+  cp -v $ORIG/$path $DEST/$dirnm/
 done
 
 for path in $SRC; do 
@@ -24,11 +26,11 @@ for path in $SRC; do
   filenm=`basename $path`
   fn="${filenm%.*}"
   pwd
-  cp -v ./$ORIG/$path ./instrumented/$dirnm/
-  $PARSE ./instrumented/$path -I./instrumented $OPTS -o ./instrumented/$dirnm/$fn.pdb   
-  $INSTRUMENT ./instrumented/$dirnm/$fn.pdb ./instrumented/$path -l -o ./instrumented/$dirnm/$fn.tau.c \
+  cp -v $ORIG/$path $DEST/$dirnm/
+  $PARSE $DEST/$path -I$DEST $OPTS -o $DEST/$dirnm/$fn.pdb   
+  $INSTRUMENT $DEST/$dirnm/$fn.pdb $DEST/$path -l -o $DEST/$dirnm/$fn.tau.c \
                -c -spec ./TAUINST.SPEC
-  mv ./instrumented/$dirnm/$fn.tau.c ./instrumented/$path
+  mv $DEST/$dirnm/$fn.tau.c $DEST/$path
 done
 
-rm -f ./instrumented/$dirnm/*.pdb
+rm -f $DEST/$dirnm/*.pdb
