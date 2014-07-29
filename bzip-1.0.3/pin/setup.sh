d1a5ac1791b5e1d1929960d5b6d3c0a7adde4a0d
Parfunc Group Account <parfunc@indiana.edu>
2014-07-29 01:35:14 -0400

    Adding pin benchmark to bzip

--- bzip-1.0.3/pin/setup.sh
+++ bzip-1.0.3/pin/setup.sh
@@ -4,4 +4,10 @@ if [ -d "../instrumented" ]; then
   rm -rf ../instrumented
 fi
 
+if [ ! -e ../../resources/a ]; then
+  cd ../../resources
+  chmod u+x filegen
+  ./filegen
+fi
+
 cp -R ../original/ ../instrumented/
