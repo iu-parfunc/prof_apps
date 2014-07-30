23ad3e06ce851a7e385bfe3d3e9fd0c312086d12
Parfunc Group Account <parfunc@indiana.edu>
2014-07-30 15:22:30 -0400

    Adding files left out

---
+++ bzip-1.0.3/setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+BENCH_HOME=`pwd`
+
+if [ -d "instrumented" ]; then
+  rm -rf instrumented
+fi
+
+if [ ! -e ../resources/a ]; then
+  cd ../resources
+  chmod u+x filegen
+  ./filegen
+fi
+
+cd $BENCH_HOME
+cp -R original instrumented
+
+if [ "$1" = "dynaprof" ]; then
+  cd ..
+  ./install.sh bzip2 
+  echo -e "\nInstalled dynaprof..."
+fi
