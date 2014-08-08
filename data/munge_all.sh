a85af784ee5ef1d2e3913cf8844f047e582f1f11
Ryan Newton <rrnewton@gmail.com>
2014-08-07 21:25:28 -0400

    Adding new results

--- data/munge_all.sh
+++ data/munge_all.sh
@@ -3,11 +3,11 @@
 benchs="bzip2 grep gzip h264"
 set -xe
 topd=`pwd`
-OUT=../output
+OUT=../../output
 
 for bench in $benchs; do
-    cd $topd/$bench
+    cd $topd/$bench/sampling
     mkdir -p $OUT
-    ../munge.py    -o $OUT/"$bench"_non_compensated.txt
-    ../munge.py -c -o $OUT/"$bench"_compensated.txt
+    ../../munge.py    -o $OUT/"$bench"_non_compensated.txt
+    ../../munge.py -c -o $OUT/"$bench"_compensated.txt
 done
