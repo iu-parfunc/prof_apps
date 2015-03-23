b8146a8381f2123cc612a2e3a50cd68ed91a5bb5
Joel Svensson <svenssonjoel@yahoo.se>
2015-03-23 15:22:16 -0400

    update

--- bzip-1.0.3/BatchBench/runit.sh
+++ bzip-1.0.3/BatchBench/runit.sh
@@ -9,11 +9,16 @@ if [ -f data_out.$RUNID ] ; then
     rm data_out.$RUNID
 fi 
 
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../../../build
+export PROFILER_TYPE=SAMPLING
+export TARGET_OVERHEAD=5
+
+
 echo "filesize, real, user, sys" > data_out.$RUNID
 
 for i in 1 10 20 30 40 50 60 70 80 90 100 ; do 
-   /usr/bin/time -f "$i, %e, %U, %S" ../instrumented/bzip2 large.random $i 2>>data_out.$RUNID
-
+    /usr/bin/time -f "$i, %e, %U, %S" -o data_out.$RUNID -a \
+      ../instrumented/bzip2 large.random $i 
    
 done 
 
