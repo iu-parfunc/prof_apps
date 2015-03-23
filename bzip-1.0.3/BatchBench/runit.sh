38b591dd124ecd81e72591d57571229224ecc58b
Joel Svensson <svenssonjoel@yahoo.se>
2015-03-23 16:39:40 -0400

    update

--- bzip-1.0.3/BatchBench/runit.sh
+++ bzip-1.0.3/BatchBench/runit.sh
@@ -16,9 +16,9 @@ export TARGET_OVERHEAD=5
 
 echo "filesize, real, user, sys" > data_out.$RUNID
 
-for i in 1 10 20 30 40 50 60 70 80 90 100 ; do 
-    /usr/bin/time -f "$i, %e, %U, %S" -o data_out.$RUNID -a \
-      ../instrumented/bzip2 large.random $i 
-   
+for i in 1 10 20 30 40 50 60 70 80 90 100 ; do
+    (cd ../instrumented; \
+     /usr/bin/time -f "$i, %e, %U, %S" -o ../BatchBench/data_out.$RUNID -a \
+        ./bzip2 ../BatchBench/large.random $i )
 done 
 
