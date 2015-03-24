098db2d1aedfbb1ff8fe2988a5fdd34f6a7b88a0
Ryan Newton <rrnewton@gmail.com>
2015-03-24 13:36:55 -0400

    update

---
+++ fluid/ManuBench/runit.sh
@@ -0,0 +1,48 @@
+
+
+RUNID=$1 
+
+EXECDIR=../src
+EXECUTABLE='./fluidanimate 8 50'
+DATAFILE=in_500K.fluid
+
+
+if [ -f data_out.$RUNID ] ; then 
+    rm data_out.$RUNID
+fi 
+
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../../../build 
+
+
+export PROFILER_TYPE=FIXED_BACKOFF 
+
+echo "variant, real, user, sys" > data_out.$RUNID 
+
+if [ $RUNID = ubiprof ] ; then
+for i in 1000000 2000000 3000000 4000000 5000000 6000000 7000000 8000000 9000000 10000000 ; do 
+    
+    export SAMPLE_SIZE=$i 
+
+    for retry in 1 2 3 4 5; do
+    (cd $EXECDIR ; \
+	/usr/bin/time -f "backoff_$i, %e, %U, %S" -o ../ManuBench/data_out.$RUNID -a \
+	   $EXECUTABLE $DATAFILE)
+    if [ $? = 0 ] ; then break 
+    fi
+    done
+
+
+done
+else 
+  (cd $EXECDIR ; \
+	/usr/bin/time -f "unprofiled %e, %U, %S" -o ../ManuBench/data_out.$RUNID -a \
+	   $EXECUTABLE $DATAFILE)
+  
+fi
+
+
+
+
+
+
