89f258eed99f238a815f025f0e1d12620d6904da
Joel Svensson <svenssonjoel@yahoo.se>
2015-03-23 23:40:31 -0400

    update

--- nbody/ManuBench/runit.sh
+++ nbody/ManuBench/runit.sh
@@ -12,18 +12,24 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../../../build
 
 export PROFILER_TYPE=FIXED_BACKOFF 
 
-echo "real, user, sys" > data_out.$RUNID 
+echo "variant, real, user, sys" > data_out.$RUNID 
 
+if [ $RUNID = ubiprof ] ; then
 for i in 1000000 2000000 3000000 4000000 5000000 6000000 7000000 8000000 9000000 10000000 ; do 
     
     export SAMPLE_SIZE=$i 
 
     (cd ../BarnesHut; \
-	/usr/bin/time -f "%e, %U, %S" -o ../ManuBench/data_out.$RUNID -a \
+	/usr/bin/time -f "backoff_$i, %e, %U, %S" -o ../ManuBench/data_out.$RUNID -a \
 	   ./nbody ../ManuBench/input.data)
     
 done
-
+else 
+  (cd ../BarnesHut; \
+	/usr/bin/time -f "unprofiled %e, %U, %S" -o ../ManuBench/data_out.$RUNID -a \
+	   ./nbody ../ManuBench/input.data)
+  
+fi
 
 
 
