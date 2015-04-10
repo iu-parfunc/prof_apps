ada3e365033ea930112b4c4f80f5da9ee98ea679
chamibuddhika <chamibuddhika@gmail.com>
2015-04-10 04:42:52 -0400

    Improvments to ground thruth analysis script

--- scripts/grok_groundtruth.py
+++ scripts/grok_groundtruth.py
@@ -31,9 +31,11 @@ def get_min_time(sample_file, function):
 
 def main():
   # benches = ['h264ref-9.3', 'perl-5.8.7', 'bzip-1.0.3',  'hmmer', 'lbm', 'nbody', 'blackscholes', 'fluid', 'hull']
-  benches = ['h264ref-9.3' , 'hmmer', 'lbm', 'perl-5.8.7', 'bzip-1.0.3', 'sjeng']
+  # benches = ['h264ref-9.3' , 'hmmer', 'lbm', 'perl-5.8.7', 'bzip-1.0.3', 'sjeng']
+  benches = ['h264ref-9.3']
   rounds = ['1', '2', '3']
-  heavy_hitters = {'h264ref-9.3' : ['FastLine16Y_11','FastPelY_14','PutPel_14', 'SATD', 'UMVLine16Y_11', 'UMVPelY'],
+  overheads = ['3', '5', '10']
+  heavy_hitters = {'h264ref-9.3' : ['FastLine16Y_11','FastPelY_14','PutPel_14', 'SATD', 'UMVLine16Y_11', 'UMVPelY_14'],
                    'hmmer' : ['FCHoose', 'Gaussrandom', 'sre_malloc', 'sre_random', 'SymbolIndex', 'toupper'],
                    'lbm' : ['LBM_initializeGrid','LBM_initializeSpecialCell','LBM_loadObstacleFile', 'LBM_showGridStatistics', 'LBM_performStreamCollide', 'LBM_swapGrids'],
                    'nbody' : ['forceTo','_vect3d<double>::operator*(double)','_vect3d<double>::operator+(_vect3d<double>, dot', '_point3d<double>::operator-(_point3d<double>)', '_vect3d<double>::_vect3d(double, double, double)', 'dot'],
@@ -44,26 +46,85 @@ def main():
                    'blackscholes' : ['main','bs_thread','BlkSchlsEqEuroNoDiv', 'CNDF'],
                    'CNDFhull' : ['cross','triArea','_vect2d', '_point2d<double>::operator-(_point2d<double>)', 'isSpace']}
 
-  RESULTS = "/u/crest-team/working_copies/onlineProfiling/groundtruth"
-  analysis = "Benchmark,Function,"
-  for round in rounds:
-    analysis += ",Min%s" % round
-  analysis += ",MinMin\n"
+  RESULTS = "/u/budkahaw/Builds/ubiprof/groundtruth"
+  # analysis = "Benchmark,Function,"
+  analysis = ""
+  # for round in rounds:
+  #   analysis += ",Min%s" % round
+  # analysis += ",MinMin\n"
 
   with cd(RESULTS):
     for bench in benches:
       for heavy_hitter in heavy_hitters.get(bench):
         analysis += "%s,%s" % (bench, heavy_hitter) 
-	min_times = []
+        min_times = []
         for round in rounds:
-          sample_file = "prof-swarm-" + bench + "-" + round + "-" + heavy_hitter + ".out"
-	  print "Processing %s..\n" % sample_file 
+          sample_file = "prof-wolverine.soic.indiana.edu-" + bench + "-" + round + "-" + heavy_hitter + ".out"
+          print "Processing %s..\n" % sample_file 
           min_times.append(get_min_time(sample_file, heavy_hitter)) 
-	  analysis += ",%s" % min_times[int(round) - 1]
+          analysis += ",%s" % min_times[int(round) - 1]
         analysis += ",%s\n" % min(min_times)	  
 
+  with open('ground.txt', 'w') as ground:
+    ground.write(analysis)
+    
   print analysis
-	  
+
+  analysis = ""
+
+  for bench in benches:
+    for overhead in overheads: 
+      analysis = ""
+      for heavy_hitter in heavy_hitters.get(bench):
+        analysis += "%s,%s" % (bench, heavy_hitter) 
+        min_times = []
+        for round in rounds:
+          sample_file = "adaptive-prof-wolverine.soic.indiana.edu-" + bench + "-" + round + "-" + overhead+ ".out"
+          print "Processing %s..\n" % sample_file 
+          min_times.append(get_min_time(sample_file, heavy_hitter)) 
+          analysis += ",%s" % min_times[int(round) - 1]
+        analysis += ",%s\n" % min(min_times)	  
+      with open('adaptive-%s.txt' % str(overhead), 'w') as ground:
+        ground.write(analysis)
+      print analysis
+
+  analysis = ""
+  for bench in benches:
+    for heavy_hitter in heavy_hitters.get(bench):
+      analysis += "%s,%s" % (bench, heavy_hitter) 
+      min_times = []
+      for round in rounds:
+        sample_file = "sampling-prof-wolverine.soic.indiana.edu-" + bench + "-" + round + ".out"
+        print "Processing %s..\n" % sample_file 
+        min_times.append(get_min_time(sample_file, heavy_hitter)) 
+        analysis += ",%s" % min_times[int(round) - 1]
+      analysis += ",%s\n" % min(min_times)	  
+
+  with open('sampling.txt', 'w') as ground:
+    ground.write(analysis)
+    
+  print analysis
+
+  analysis = ""
+  for bench in benches:
+    for heavy_hitter in heavy_hitters.get(bench):
+      analysis += "%s,%s" % (bench, heavy_hitter) 
+      min_times = []
+      for round in rounds:
+        sample_file = "backoff-prof-wolverine.soic.indiana.edu-" + bench + "-" + round + ".out"
+        print "Processing %s..\n" % sample_file 
+        min_times.append(get_min_time(sample_file, heavy_hitter)) 
+        analysis += ",%s" % min_times[int(round) - 1]
+      analysis += ",%s\n" % min(min_times)	  
+
+  with open('backoff.txt', 'w') as ground:
+    ground.write(analysis)
+    
+  print analysis
+  
+
+
+
 
 if __name__ == "__main__":
       main()
