d55f3ad77f5a14cd47bcbf228357e3ea9347d272
budkahaw <budkahaw@indiana.edu>
2016-08-12 01:15:52 -0400

    Adding makefiles for building unprofiled versions

--- lbm/setup.sh
+++ lbm/setup.sh
@@ -2,12 +2,12 @@
 
 set -e 
 
-if [ -d "instrumented" ]; then
+if [[ -d "instrumented" ]]; then
   rm -rf instrumented
 fi
 
 cp -R original instrumented
 
-
-
-
+if [[ $1 == "liteprof" ]]; then
+  cp liteprof/main.c instrumented/main.c
+fi
